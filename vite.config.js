6e63bf2d99d6807b54d48c5f95f2bcd2c930f08a
Danila2131A <stimakkt@gmail.com>
2025-08-01 00:41:41 +0500

    fix: set correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/google-api/', // <-- Вот эта строка всё исправит!
   plugins: [react()],
 })
