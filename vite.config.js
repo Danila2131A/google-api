6e18f8f77403621c4a1c97d11dc68de969d1c65e
Danila2131A <stimakkt@gmail.com>
2025-08-01 13:00:46 +0500

    Configure gh-pages deployment, clean index.html and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/google-api/', // <-- Вот эта строка всё исправит!
   plugins: [react()],
-})
+  base: '/google-api/', // Убедитесь, что 'google-api' - это точное имя вашего репозитория на GitHub
+});
